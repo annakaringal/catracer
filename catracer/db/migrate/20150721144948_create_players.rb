d17b799f1af90d91f91d4a5b041e932cfdeaed15
anna karingal <hello@annakaringal.com>
2015-07-21 10:52:42 -0400

    Create players and games table migration

--- catracer/db/migrate/20150721144948_create_players.rb
+++ catracer/db/migrate/20150721144948_create_players.rb
@@ -1,7 +1,8 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
-
+      t.string :name, null: false
+      t.integer :score, default: 0
       t.timestamps null: false
     end
   end
--- catracer/db/migrate/20150721144958_create_games.rb
+++ catracer/db/migrate/20150721144958_create_games.rb
@@ -1,7 +1,11 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-
+      t.integer :player1_id
+      t.integer :player2_id
+      t.timestamps :time_to_complete
+      t.boolean :completed, default: false
+      t.integer :winner_id
       t.timestamps null: false
     end
   end
