3b77df9315e1b0d6f8928e2c47d1a65598eebbd0
anna karingal <hello@annakaringal.com>
2015-07-21 11:20:05 -0400

    Implement create and show routes for game

--- catracer/app/controllers/games_controller.rb
+++ catracer/app/controllers/games_controller.rb
@@ -8,9 +8,21 @@ class GamesController < ActionController::Base
   end
 
   def create
+    @game = Game.new
+    @game.player1 = Player.find_or_create_by(name: params[:player1_name])
+    @game.player2 = Player.find_or_create_by(name: params[:player2_name])
+    if @game.save
+      redirect_to @game
+    else
+      flash[:error] = "There was an error creating your game!"
+      redirect_to new_game_path
+    end
   end
 
   def show
+    @game = Game.find_by(id: params[:id])
+    @player1 = game.player1
+    @player2 = game.player2
   end
 
   def update
